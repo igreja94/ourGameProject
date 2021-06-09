ed4d38f3b4ac5dd76f0425e7bdc36d29f3655950
thomazkopanakis <reginaazevedo@gmail.com>
2021-06-09 18:29:00 +0100

    new update

---
+++ Animal Reign/src/Cards/Normal/Bear.java
@@ -0,0 +1,118 @@
+package Cards.Normal;
+
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Bear extends NormalCard{
+
+    public Bear() {
+        super("Bear");
+        animalPicture = new Picture(550, 85, "BEAR.jpg");
+        animalPicture.draw();
+        keyboardInit();
+
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+
+    @Override
+    public void actionOne() {
+        super.actionOne();
+        Picture question = new Picture();
+        question.draw();
+        keyboardInit();
+        if (isYes) {
+            executeActionYes();
+        } else {
+            executeActionNo();
+        }
+        question.delete();
+    }
+
+    @Override
+    public void actionTwo() {
+        super.actionTwo();
+        Picture question = new Picture();
+        question.draw();
+        keyboardInit();
+        if (isYes) {
+            executeActionYes();
+        } else {
+            executeActionNo();
+        }
+        question.delete();
+
+    }
+
+    @Override
+    public void actionThree() {
+        super.actionThree();
+        Picture question = new Picture();
+        question.draw();
+        keyboardInit();
+        if (isYes) {
+            executeActionYes();
+        } else {
+            executeActionNo();
+        }
+        question.delete();
+    }
+
+    @Override
+    public void actionFour() {
+        super.actionFour();
+        Picture question = new Picture();
+        question.draw();
+        keyboardInit();
+        if (isYes) {
+            executeActionYes();
+        } else {
+            executeActionNo();
+        }
+        question.delete();
+    }
+
+    @Override
+    public void executeActionYes() {
+        super.executeActionYes();
+    }
+
+    @Override
+    public void executeActionNo() {
+        super.executeActionNo();
+    }
+
+    @Override
+    public void cardSelected() {
+        super.cardSelected();
+        animalString = new Picture();
+        actionRandomizer();
+        keyboardInit();
+    }
+
+    @Override
+    public void actionRandomizer() {
+        int randomize = (int) ((Math.random() * 4) + 1);
+
+        switch (randomize) {
+            case 1:
+                actionOne();
+                break;
+            case 2:
+                actionTwo();
+                break;
+            case 3:
+                actionThree();
+                break;
+            case 4:
+                actionFour();
+                break;
+            default:
+                System.out.println("Something unexpected happened!");
+        }
+
+    }
+}
